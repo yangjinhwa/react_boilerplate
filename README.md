3d82161b3ed3842a04e53a019ad95ed2bc3f2f6c
ginna <ginna9498@gmail.com>
2018-04-08 21:10:50 +0900

    html-webpack-plugin 설정

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ npm init
 yarn add webpack --dev
 yarn add -D webpack-cli
 yarn start
-
+yarn add --dev html-webpack-plugin
 ```
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './1.js',
   output: {
   path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js'
-}
+    filename: 'bundle2.js'
+},
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    })
+  ]
 };
